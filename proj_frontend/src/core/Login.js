b352e308b3a661956575e66034042a4ea4f6e3c8
sankt <bsankt16@gmail.com>
2020-08-24 18:56:51 +0530

    login form

--- proj_frontend/src/core/Login.js
+++ proj_frontend/src/core/Login.js
@@ -2,9 +2,17 @@ import React from 'react'
 import Nav from './Nav'
 
 const Login = () => {
+
+    const loginForm = () => (
+        <>
+            <h1>Hello there!</h1>
+        </>
+    )
+
     return (
         <>
             <Nav />
+            {loginForm()}
         </>
     )
 }
